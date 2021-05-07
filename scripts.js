160aa3460494c5102021d82f75a9aa2ee7dd1381
Josh Come Ibad <joshcibad@gmail.com>
2021-05-06 21:23:22 -0700

    Update

--- scripts.js
+++ scripts.js
@@ -8,8 +8,6 @@ function drawGrid(elemId, dotColor, numX=9){
 	var height = oldHeight*10;
 	tmpCanvas.width = width;
 	tmpCanvas.height = height;
-	console.log(width);
-	console.log(height);
 	dotX = width / (4*numX);
 	
 	for(var x=dotX+2; x<width; x+=dotX*4){
@@ -27,11 +25,14 @@ function drawGrid(elemId, dotColor, numX=9){
 
 function makeNavbar(activePage){
 	var navBar = document.getElementById("main-navbar");
-	navBar.innerHTML += '<ul><li><a href="index.html" class="logo"></a></li>'+
-		'<li><a href="our_technology.html"' + ((activePage==1) ? ' class="active"' : '') + '>Technology</a></li>'+
+	navBar.innerHTML += '<div class="logoName"><a href="index.html" class="logo"></a>'+
+		'<a href="index.html" class="companyName">NoetherTech</a></div>'+
+		'<ul id="main-navbar-navigation"><li><a href="our_technology.html"' + ((activePage==1) ? ' class="active"' : '') + '>Technology</a></li>'+
 		'<li><a href="science.html"' + ((activePage==2) ? ' class="active"' : '') + '>Science</a></li>'+
 		'<li><a href="history.html"' + ((activePage==3) ? ' class="active"' : '') + '>Company</a></li>'+
-		'</ul><span class="contactUs"><a class="button" href="">CONTACT US</a></span>';
+		'<li id="contactUsButton"><a href="">Contact Us</a></li>'+
+		'</ul><span id="contactUs"><a class="button" href="">CONTACT US</a></span>'+
+		'<a onclick="showMenuBar()" id="dropdown"></a>';
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -46,3 +47,16 @@ function scrollHandler() {
 }
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {scrollHandler()};
+
+
+function showMenuBar(){
+	var navbar = document.getElementById("main-navbar-navigation");
+	var contactUs = document.getElementById("contactUsButton");
+	if(navbar.style.display == "none" || navbar.style.display == ""){
+		navbar.style.display = "inline-block";
+		contactUs.style.display = "inline-block";
+	}else{
+		navbar.style.display = "none";
+		contactUs.style.display = "none";
+	}
+}
